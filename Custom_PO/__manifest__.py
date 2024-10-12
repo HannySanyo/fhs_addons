18bcf81287ade65dd18cba854dae5ebb372aa0a4
Hanny <hanny@federalheatingsupply.com>
2024-10-12 13:20:42 -0600

    baseline

---
+++ Custom_PO/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'FHS-custom_PO',
+    'version': '1.0',
+    'category': 'Purchases',
+    'depends': ['purchase'],
+    'data': [
+        'views/purchase_order_view.xml',
+    ],
+}
