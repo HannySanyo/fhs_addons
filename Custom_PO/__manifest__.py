a83a803ab0e648fc38a83d5d62bff8bfa57c657f
Hanny <hanny@federalheatingsupply.com>
2024-10-12 21:20:00 -0600

    newbaseline

--- Custom_PO/__manifest__.py
+++ Custom_PO/__manifest__.py
@@ -1,17 +1,12 @@
 {
     'name': 'Custom Purchase Order Report',
     'version': '1.0',
-    'category': 'Purchase',
-    'summary': 'Customizations to the Purchase Order report',
-    'description': """
-        This module customizes the purchase order report to include custom shipping address information.
-    """,
-    'author': 'Your Name',
-    'depends': ['purchase', 'purchase_stock'], 
+    'category': 'Purchases',
+    'summary': 'Customizations to Purchase Order Report',
+    'depends': ['purchase', 'web'],
     'data': [
-        'views/purchase_order_view.xml', 
+        'views/report_purchase_order.xml',
     ],
     'installable': True,
     'application': False,
-    'auto_install': False,
 }
