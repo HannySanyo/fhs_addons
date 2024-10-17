299178e9f861cd2c4ac1ea3566a1929e5f297bc0
Grace Sanyo <grace.federalheatingsupply@gmail.com>
2024-10-16 20:57:14 -0600

    Clean up main

--- fhs_acct_signup_extension/controllers/main.py
+++ fhs_acct_signup_extension/controllers/main.py
@@ -13,12 +13,12 @@ from odoo.http import request, route
 from odoo.exceptions import UserError
 from odoo.addons.web.controllers.home import SIGN_UP_REQUEST_PARAMS
 from odoo.addons.auth_signup.controllers.main import AuthSignupHome
-from odoo import SUPERUSER_ID  # Import SUPERUSER_ID
+from odoo import SUPERUSER_ID
 
 class AuthSignupHomeInherit(AuthSignupHome):
 
     def do_signup(self, qcontext):
-        """ Shared helper that creates a res.partner out of a token """
+
         values = {key: qcontext.get(key) for key in (
             'login',
             'name',
@@ -43,19 +43,19 @@ class AuthSignupHomeInherit(AuthSignupHome):
         
         if values.get('address_str1'):
             values.update({
+                'street': values.get('address_str1'),
+                'street2': values.get('address_str2'),
                 'city': values.get('city'),
                 'state_id': values.get('state_id'),
                 'zip': values.get('zip'),
-                'street': values.get('address_str1'),
-                'street2': values.get('address_str2'),
             })
 
         if values.get('fiscal_pos_doc_name'):
             datas = base64.b64encode(values.get('fiscal_pos_doc').read())
             filename = values.get('fiscal_pos_doc').filename
             values.update({
-                'fiscal_pos_doc': datas,
-                'fiscal_pos_doc_name': filename,
+                #'fiscal_pos_doc': datas,
+                #'fiscal_pos_doc_name': filename,
                 'x_studio_fiscal_doc': datas,
                 'x_studio_fiscal_doc_filename': filename
             })
@@ -64,8 +64,8 @@ class AuthSignupHomeInherit(AuthSignupHome):
             datas = base64.b64encode(values.get('contractor_doc').read())
             filename = values.get('contractor_doc').filename
             values.update({
-                'contractor_doc': datas,
-                'contractor_doc_filename': filename,
+                #'contractor_doc': datas,
+                #'contractor_doc_filename': filename,
                 'x_studio_contractor_doc': datas,
                 'x_studio_contractor_doc_filename': filename
             })
