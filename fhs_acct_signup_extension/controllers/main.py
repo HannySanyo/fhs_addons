a3fcff0ae4715daa0f90e94cb6de329328c77087
Grace Sanyo <grace.federalheatingsupply@gmail.com>
2024-10-17 21:39:09 -0600

    What's going on with the emails?

--- fhs_acct_signup_extension/controllers/main.py
+++ fhs_acct_signup_extension/controllers/main.py
@@ -1,3 +1,11 @@
+#################################################################################
+# File Name: main.py
+# Revision History:  Engineer    Date          Description
+#                    G. Sanyo    10/14/2024    Add address fields
+#                    G. Sanyo    10/03/2024    Add attachments for contractor doc
+#                                              and fiscal position doc
+#                    G. Sanyo    09/29/2024    Creation
+#################################################################################
 import base64
 from odoo import _
 from odoo import http
@@ -9,6 +17,7 @@ from odoo.addons.auth_signup.controllers.main import AuthSignupHome
 class AuthSignupHomeInherit(AuthSignupHome):
 
     def do_signup(self, qcontext):
+
         values = {key: qcontext.get(key) for key in (
             'login',
             'name',
@@ -27,11 +36,10 @@ class AuthSignupHomeInherit(AuthSignupHome):
 
         if not values:
             raise UserError(_("The form was not properly filled in."))
-
+        
         if values.get('password') != qcontext.get('confirm_password'):
             raise UserError(_("Passwords do not match; please retype them."))
-
-        # Update address fields
+        
         if values.get('address_str1'):
             values.update({
                 'street': values.get('address_str1'),
@@ -41,37 +49,27 @@ class AuthSignupHomeInherit(AuthSignupHome):
                 'zip': values.get('zip'),
             })
 
-        # Handle fiscal document upload
-        if values.get('fiscal_pos_doc'):
-            if hasattr(values['fiscal_pos_doc'], 'read'):
-                datas = base64.b64encode(values['fiscal_pos_doc'].read()).decode('utf-8')
-                filename = values['fiscal_pos_doc'].filename
-                values.update({
-                    'x_studio_fiscal_doc': datas,
-                    'x_studio_fiscal_doc_filename': filename
-                })
-            else:
-                raise UserError(_("Invalid fiscal position document uploaded."))
+        if values.get('fiscal_pos_doc_name'):
+            datas = base64.b64encode(values.get('fiscal_pos_doc').read())
+            filename = values.get('fiscal_pos_doc').filename
+            values.update({
+                'x_studio_fiscal_doc': datas,
+                'x_studio_fiscal_doc_filename': filename
+            })
 
-        # Handle contractor document upload
         if values.get('contractor_doc'):
-            if hasattr(values['contractor_doc'], 'read'):
-                datas = base64.b64encode(values['contractor_doc'].read()).decode('utf-8')
-                filename = values['contractor_doc'].filename
-                values.update({
-                    'x_studio_contractor_doc': datas,
-                    'x_studio_contractor_doc_filename': filename
-                })
-            else:
-                raise UserError(_("Invalid contractor document uploaded."))
+            datas = base64.b64encode(values.get('contractor_doc').read())
+            filename = values.get('contractor_doc').filename
+            values.update({
+                'x_studio_contractor_doc': datas,
+                'x_studio_contractor_doc_filename': filename
+            })
 
-        # Set language
         supported_lang_codes = [code for code, _ in request.env['res.lang'].get_installed()]
         lang = request.env.context.get('lang', '').split('_')[0]
         if lang in supported_lang_codes:
             values['lang'] = lang
-
-        # Perform signup
+            
         self._signup_with_values(qcontext.get('token'), values)
         request.env.cr.commit()
 
@@ -88,6 +86,7 @@ class AuthSignupHomeInherit(AuthSignupHome):
         else:
             qcontext['states'] = request.env['res.country.state'].sudo().browse([])  # No states if country not found
 
+
         SIGN_UP_REQUEST_PARAMS.update({
             'phone',
             'address_str1',
@@ -101,4 +100,4 @@ class AuthSignupHomeInherit(AuthSignupHome):
             'contractor_doc_filename'
         })
 
-        return qcontext
+        return super().get_auth_signup_qcontext()
